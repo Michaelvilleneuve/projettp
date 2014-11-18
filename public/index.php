04f3cbcab318b75f3cad277214fd9da808cfa07c
VILLENEUVE <contact@michaelvilleneuve.fr>
2014-11-18 15:52:15 +0100

    ca marche enfin

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php 
+$datas = array(
+    'title' => 'Michael et Octave',
+    'name' => 'world',
+    'menulink' => array(
+        'login' => 'login.php',
+        'register' => 'register.php',
+        'home' => 'index.php'
+    ),
+    'current' => 'home',
+    'tab' => array('Login', 'Register', 'Accueil')
+);
 require_once '../vendor/autoload.php';
-$loader = new Twig_Loader_File('../application/views');
+$loader = new Twig_Loader_Filesystem('../application/views');
 $twig = new Twig_Environment($loader);
-echo $twig->render('index.twig', array('name'=>'Michael'));
+echo $twig->render('index.twig', $datas);
